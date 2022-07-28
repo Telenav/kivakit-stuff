a003b55df9102d896beaa80333f64ed584e20652
Tim Boudreau <timb@telenav.com>
2022-07-28 19:59:41 -0400

    Works - at least as far as building.  None of this is a good idea, but it does make the virulently module-system-hostile hadoop dependencies usable.

--- kivakit-hdfs-filesystem/hdfs-proxy/src/main/java/module-info.java
+++ kivakit-hdfs-filesystem/hdfs-proxy/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module kivakit.filesystems.hdfs.proxy {
-    requires transitive kivakit.application;
-    requires transitive kivakit.filesystems.hdfs.proxy.spi;
+//    requires transitive kivakit.application;
+//    requires transitive kivakit.filesystems.hdfs.proxy.spi;
     requires kivakit.hadoop.doubly.shaded.protobuffer;
     requires java.rmi;
 }
